d484cd6214cc2bdc3f370ece0c6f9371e265816f
Ryan <ryanpinas77@gmail.com>
2025-05-19 15:31:23 +0200

    feat(backend): initialiser le projet avec Gin et une route de test
    - Mise en place de la structure de base du projet Go
    - Configuration du framework Gin
    - Ajout d'une route de test /ping

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Exemple de route de test
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(":8080") // localhost:8080
+}
